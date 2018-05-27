83681970641d8d1c700330ec8e7ff69672675a64
Sam Gardner <stgardner4@att.net>
2018-05-26 21:52:15 -0500

    Made basic UI

--- chronos-installer/AppDelegate.h
+++ chronos-installer/AppDelegate.h
@@ -3,7 +3,7 @@
 //  chronos-installer
 //
 //  Created by Sam Gardner on 5/11/18.
-//  Copyright © 2018 Sam Gardner. All rights reserved.
+//  Copyright © 2018 Chronos Development. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
---
+++ chronos-installer/HomePageViewController.m
@@ -0,0 +1,38 @@
+//
+//  ViewController.m
+//  chronos-installer
+//
+//  Created by Sam Gardner on 5/11/18.
+//  Copyright © 2018 Chronos Development. All rights reserved.
+//
+
+#import "HomePageViewController.h"
+
+@interface HomePageViewController ()
+
+@end
+
+@implementation HomePageViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self.navigationController setNavigationBarHidden:TRUE animated:FALSE];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+- (IBAction)infoButton:(id)sender {
+    UIAlertController *teamInfo = [UIAlertController alertControllerWithTitle:@"Chronos Installer" message:@"Created by EthanR, FoxletFox, Lepidus, PsychoTea, and Samg_is_a_Ninja" preferredStyle:UIAlertControllerStyleAlert];
+    UIAlertAction *okAction = [UIAlertAction actionWithTitle:@"Dismiss" style:UIAlertActionStyleDefault handler:nil];
+    [teamInfo addAction:okAction];
+    [self presentViewController:teamInfo animated:YES completion:nil];
+}
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    [self.navigationController setNavigationBarHidden:FALSE animated:TRUE];
+}
+
+
+@end
