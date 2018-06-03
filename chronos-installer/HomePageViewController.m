279ec7766c0e493444e24ef9d9965097bcef9f6e
Sam Gardner <stgardner4@att.net>
2018-06-03 16:12:15 -0500

    Added source refreshing

--- chronos-installer/HomePageViewController.m
+++ chronos-installer/HomePageViewController.m
@@ -7,6 +7,9 @@
 //
 
 #import "HomePageViewController.h"
+#import <spawn.h>
+#include <sys/sysctl.h>
+extern char **environ;
 
 @interface HomePageViewController ()
 
@@ -18,6 +21,16 @@
     [super viewDidLoad];
     [self.navigationController setNavigationBarHidden:TRUE animated:FALSE];
     // Do any additional setup after loading the view, typically from a nib.
+    self.refreshProgressBar.progress = 0;
+    [_refreshProgressBar setHidden:TRUE];
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *documentsDirectory = [paths objectAtIndex:0];
+    NSString *sourcesplistFilePath = [documentsDirectory stringByAppendingPathComponent:@"sources.plist"];
+    if ([[NSFileManager defaultManager] fileExistsAtPath:sourcesplistFilePath]) {
+    } else {
+        [_refreshProgressLabel setHidden:TRUE];
+        [_refreshProgressLabel setEnabled:FALSE];
+    }
 }
 
 
@@ -27,9 +40,51 @@
     [teamInfo addAction:okAction];
     [self presentViewController:teamInfo animated:YES completion:nil];
 }
+- (IBAction)refreshSourcesButton:(id)sender {
+    [_refreshProgressLabel setEnabled:FALSE];
+    [_refreshProgressLabel setTintColor:[UIColor darkGrayColor]];
+    [_refreshProgressLabel setTitle:@"Reading sources" forState:UIControlStateNormal];
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *documentsDirectory = [paths objectAtIndex:0];
+    NSString *sourcesplistFilePath = [documentsDirectory stringByAppendingPathComponent:@"sources.plist"];
+    _sourceList = [NSArray arrayWithContentsOfFile:sourcesplistFilePath];
+    _sourceBeingUpdated = 0;
+    [self downloadPackagesFile];
+}
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
     [self.navigationController setNavigationBarHidden:FALSE animated:TRUE];
 }
+- (void) URLSession:(NSURLSession *)session downloadTask:(nonnull NSURLSessionDownloadTask *)downloadTask didWriteData:(int64_t)bytesWritten totalBytesWritten:(int64_t)totalBytesWritten totalBytesExpectedToWrite:(int64_t)totalBytesExpectedToWrite {
+    float totalSize = (totalBytesExpectedToWrite/1024)/1024.f;
+    float writtenSize = (totalBytesWritten/1024)/1024.f;
+    float downloadProgress = (writtenSize/totalSize);
+    self.refreshProgressBar.progress = downloadProgress;
+}
+-(void)downloadPackagesFile{
+    NSLog(@"Updating: %d",_sourceBeingUpdated);
+    [_refreshProgressLabel setTitle:[NSString stringWithFormat:@"Downloading package info from %@", [_sourceList objectAtIndex:_sourceBeingUpdated]] forState:UIControlStateNormal];
+    NSURL *sourceURL = [NSURL URLWithString:[_sourceList objectAtIndex:_sourceBeingUpdated]];
+    NSURLSessionDownloadTask *task = [[NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration] delegate:self delegateQueue:[NSOperationQueue mainQueue]] downloadTaskWithURL:sourceURL];
+    [_refreshProgressBar setHidden:FALSE];
+    [task resume];
+}
+- (void) URLSession:(NSURLSession *)session downloadTask:(NSURLSessionDownloadTask *)downloadTask didFinishDownloadingToURL:(NSURL *)location {
+    [_refreshProgressLabel setTitle:[NSString stringWithFormat:@"Updating source %@", [_sourceList objectAtIndex:_sourceBeingUpdated]] forState:UIControlStateNormal];
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *documentsDirectory = [paths objectAtIndex:0];
+    NSString *sourcePackagesFilePath = [documentsDirectory stringByAppendingPathComponent:[NSString stringWithFormat:@"%@-packages.plist", [[_sourceList objectAtIndex:_sourceBeingUpdated] stringByReplacingOccurrencesOfString:@"/" withString:@""]]];
+    [[NSFileManager defaultManager] moveItemAtPath:[location path] toPath:sourcePackagesFilePath error:nil];
+    if (_sourceBeingUpdated == ([_sourceList count] - 1) ) {
+        [_refreshProgressLabel setTitle:@"Refresh Sources" forState:UIControlStateNormal];
+        [_refreshProgressLabel setEnabled:TRUE];
+        [_refreshProgressLabel setTintColor:[UIColor colorWithRed:0/255.0 green:117.0/255.0 blue:251.0/255.0 alpha:1.0]];
+        self.refreshProgressBar.progress = 0;
+        [_refreshProgressBar setHidden:TRUE];
+    }
+    else {
+        _sourceBeingUpdated = _sourceBeingUpdated + 1;
+        [self downloadPackagesFile];
+    }
 
-
+}
 @end
